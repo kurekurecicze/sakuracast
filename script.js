0f0051772323ab99a2c14c2dd481dc382cedf52d
kurekurecicze <julcazakova@gmail.com>
2026-06-05 23:40:32 +0200

    Create script.js
    .

---
+++ script.js
@@ -0,0 +1,18 @@
+// 🌸 SakuraCast Global Script
+
+function toggleTheme(){
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+    } else {
+        localStorage.setItem("theme","light");
+    }
+}
+
+// načtení uloženého theme
+window.onload = function(){
+    if(localStorage.getItem("theme") === "dark"){
+        document.body.classList.add("dark");
+    }
+}
